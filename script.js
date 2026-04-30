472dc38e89aa36fa7f9c1ee718e2053a791e2fd4
anshkumargupta24052010-ship-it <anshkumargupta24052010@gmail.com>
2026-04-30 15:02:17 +0530

    Refactor scroll and card selection functions

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ function scrollToSection() {
   document.getElementById("fees").scrollIntoView({ behavior: "smooth" });
 }
 
-// selectable cards
+// card select effect
 function selectCard(card) {
   document.querySelectorAll(".card").forEach(c => c.classList.remove("highlight"));
   card.classList.add("highlight");
+}
+// show thank you screen
+function showThankYou() {
+  document.getElementById("thankyouScreen").style.display = "flex";
+}
+
+// hide thank you screen
+function closeThankYou() {
+  document.getElementById("thankyouScreen").style.display = "none";
 }
